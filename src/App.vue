50a0399ad350412a7820f0cd22cb425bedfa1a71
lim-enhui <lim.enhui@outlook.com>
2017-10-16 17:54:52 +0800

    Initial Project

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="app">
+    <div class="container">
+      <div v-for="row in rows" class="columns">
+          <app-card v-for="column in columns"></app-card>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import Card from './components/card'
+
+export default {
+  name: 'app',
+  data: function () {
+    return {
+      rows: 4,
+      columns: 4
+    }
+  },
+  components: {
+    'appCard': Card
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/card.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="column">
+    <p class="bd-notification is-info">Default Text</p>
+  </div>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
